8c0d3801f49f572df039aefdcab622039a88b6e2
Gianluca Amato <gianluca.amato.74@gmail.com>
2022-10-08 18:54:03 +0200

    Add Montecarlo ES policy valuation and control.

--- environment.py
+++ environment.py
@@ -1,4 +1,3 @@
-from __future__ import annotations
 from typing import Generic, TypeVar, NewType, Dict, List, Tuple, Iterable
 from typing import Optional, MutableMapping
 from abc import abstractmethod, ABC
@@ -12,6 +11,7 @@ Probability = NewType('Probability', float)
 Policy = Dict[S, List[Tuple[Probability, A]]]
 Episode = List[Tuple[S, A, Reward]]
 StateValue = MutableMapping[S, float]
+StateActionValue = MutableMapping[Tuple[S, A], float]
 
 def policy_action_selection(pi: Policy[S, A], s: S) -> A:
     """Returns a random action following a policy"""
@@ -56,11 +56,16 @@ class Environment(Generic[S, A], ABC):
             policy[s] = [(Probability(1/n), a) for a in actions]
         return policy
 
-    def generate_episode(self, pi: Policy[S, A], initial_state: Optional[S] = None) -> Episode[S, A]:
-        """Generate an episode for the environment following the given policy and an
-        optional initial state."""
+    def generate_episode(self, pi: Policy[S, A], initial_state: Optional[S] = None,
+                         initial_action: Optional[A] = None) -> Episode[S, A]:
+        """Generate an episode for the environment following the given policy and
+        optional initial state and initial action."""
         episode: Episode[S, A] = []
         state = self.initial_state() if initial_state is None else initial_state
+        if initial_action:
+            reward, newstate = self.interaction(state, initial_action)
+            episode.append((state, initial_action, reward))
+            state = newstate
         while not self.is_final(state):
             action = policy_action_selection(pi, state)
             reward, newstate = self.interaction(state, action)
--- montecarlo.py
+++ montecarlo.py
@@ -1,4 +1,8 @@
-from environment import S, A, Environment, Policy, StateValue
+import random
+import math
+
+from typing import Optional
+from environment import S, A, Environment, Policy, StateValue, StateActionValue, Probability
 
 def policy_evaluation(env: Environment[S, A], pi: Policy[S, A], gamma: float, iterates: int) -> StateValue[S]:
     """Evaluate a policy using Monte Carlo methods"""
@@ -15,3 +19,46 @@ def policy_evaluation(env: Environment[S, A], pi: Policy[S, A], gamma: float, it
             numvisits[state] += 1
             v[state] += (G - v[state])/numvisits[state]
     return v
+
+def policy_evaluation_es(env: Environment[S, A], pi: Policy[S, A], gamma: float,
+                         iterates: int) -> StateActionValue[S, A]:
+    """Evaluate a policy using Monte Carlo methods and Exploring Starts method"""
+    stateactions = [(s, a)  for s in env.states() for a in env.actions(s)]
+    v = {(s, a) : 0.0 for s, a in stateactions}
+    numvisits = {(s, a) : 0 for s, a in stateactions}
+    for _i in range(iterates):
+        G = 0.0
+        s0, a0 = random.choice(stateactions)
+        episode = env.generate_episode(pi, initial_state=s0, initial_action=a0)
+        for i in range(len(episode)-1, -1, -1):
+            state, action, reward = episode[i]
+            G = G*gamma + reward
+            if any(s == state and a == action for s, a, r in episode[0:i]):
+                continue
+            numvisits[state, action] += 1
+            v[state, action] += (G - v[state, action])/numvisits[state, action]
+    return v
+
+def montecarlo_es(env: Environment[S, A], gamma: float, iterates: int,
+                                  pi: Optional[Policy[S, A]] = None) -> Policy[S, A]:
+    """Determine optimal policy using the Montecarlo Exploring Starts method. It only works if
+       every policy is guaranteed to reach a terminal state."""
+    stateactions = [(s, a) for s in env.states() for a in env.actions(s)]
+    v = {(s, a) : 0.0 for s, a in stateactions}
+    numvisits = {(s, a) : 0 for s, a in stateactions}
+    if pi is None:
+        pi = env.random_policy()
+    for _i in range(iterates):
+        G = 0.0
+        s0, a0 = random.choice(stateactions)
+        episode = env.generate_episode(pi, initial_state=s0, initial_action=a0)
+        for i in range(len(episode)-1, -1, -1):
+            state, action, reward = episode[i]
+            G = G*gamma + reward
+            if any(s == state and a == action for s, a, r in episode[0:i]):
+                continue
+            numvisits[state, action] += 1
+            v[state, action] += (G - v[state, action])/numvisits[state, action]
+            pi[state] = [(Probability(1.0), max(env.actions(state), key=lambda a: v[state, a]))]
+        print(pi)
+    return pi
